abccc5cc31cb46767d1f04767fbf01016afb9bc9
Lauty128 <lautarosilverii@gmail.com>
2023-05-18 11:23:30 -0300

    error in url to make fetch fixed

--- minify/js/request.js
+++ minify/js/request.js
@@ -1,9 +1,7 @@
-const domain_url = 'http://localhost:4000'
-
 export async function getArticles({page=0, size=4}){
 
     try{
-        const url = `${domain_url}/api/articles?page=${page}&size=${size}`
+        const url = `/api/articles?page=${page}&size=${size}`
         const data = await fetch(url)
                         .then(res=> res.json()) 
 
@@ -17,7 +15,7 @@ export async function getArticles({page=0, size=4}){
 
 export async function getBooks(){
     try{
-        const url = `${domain_url}/api/books`
+        const url = `/api/books`
         const data = await fetch(url)
                         .then(res=> res.json()) 
 
