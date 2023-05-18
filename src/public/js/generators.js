d76b83cd6d917a3968541ec8e3395eeac56e4bb8
Lauty128 <lautarosilverii@gmail.com>
2023-05-18 08:24:25 -0300

    pagination styles updated

--- src/public/js/generators.js
+++ src/public/js/generators.js
@@ -205,8 +205,8 @@ export class ArticlesHandler{
 
         //---- Content
         number.textContent = this.page + 1
-        arrowLeft.innerHTML = '<svg width="35px" height="35px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M15 6l-6 6 6 6" stroke="#000000" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
-        arrowRight.innerHTML = '<svg width="35px" height="35px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M9 6l6 6-6 6" stroke="#000000" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
+        arrowLeft.innerHTML = '<svg width="30px" height="30px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M15 6l-6 6 6 6" stroke="#000000" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
+        arrowRight.innerHTML = '<svg width="30px" height="30px" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M9 6l6 6-6 6" stroke="#000000" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
 
         //---- Append
         if(this.isPrevPage) container.appendChild(arrowLeft)
