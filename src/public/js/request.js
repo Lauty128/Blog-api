51f9903e712ddc5e799431a03bf812a020d0548e
Lauty128 <lautarosilverii@gmail.com>
2023-05-26 00:07:40 -0300

    article styles changed and .js files minified with webpack

--- src/public/js/request.js
+++
@@ -1,1 +0,0 @@
-export async function getArticles(r,t=4){try{let e=`/api/articles?page=${r}&size=${t}`,a=await fetch(e).then(r=>r.json());return a}catch(n){Error("Ocurrio un error durante la operacion")}}export async function getBooks(){try{let r=await fetch("/api/books").then(r=>r.json());return r}catch(t){Error("Ocurrio un error durante la operacion")}}
