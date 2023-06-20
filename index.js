488d1920b40387dcad48f10dbd7d0e83449fe1f7
Lauty128 <lautarosilverii@gmail.com>
2023-06-20 17:48:09 -0300

    frontend deleted of project (only work on API) ---- ejs & ejs-layouts deleted and updated configuration for not working with template engines

--- index.js
+++ index.js
@@ -2,33 +2,24 @@
     import express from 'express';
     import morgan from "morgan";
     import cors from 'cors'
-    import expressEjsLayouts from 'express-ejs-layouts';
-    import path from 'path';
-    import * as url from 'url';
+    import compression from 'compression'
+    import { db_environment } from './src/config/env.js';
     
 //------ Config
     const app = express()
-    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
     const PORT = process.env.PORT || 4000
-
-//------ Template Engine
-    app.set('view engine', 'ejs');
-    app.set('views', './src/views')
     
 //------ Middlewares
     app.use(cors({ origin:['https://lautarosilverii.tech', 'http://localhost:5173']}))
-    app.use(morgan('dev'));
+    app.use(compression())
     app.use(express.json())
     app.use(express.urlencoded({ extended: false }))
-    app.use(expressEjsLayouts)  // Layouts for ejs
-    app.use(express.static(path.join(__dirname, 'src/public'))) // Public folder config
+    if(db_environment === 'dev') app.use(morgan('dev'));
     
 //------ Routes
     import { api_router } from './src/routes/api.routes.js'
-    import { pages_router } from './src/routes/pages.routes.js'
     
     app.use('/api', api_router)
-    app.use('/', pages_router)
 
 //------ Listen
 
