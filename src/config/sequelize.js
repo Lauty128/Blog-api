1b52ecb2fdc5daa2a26b1809fb8075425ff26d7d
Lauty128 <lautarosilverii@gmail.com>
2023-08-05 11:24:24 -0300

    Message for dev mode and pool connection cofigured in production mode

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,14 +4,23 @@
 //---- Config
     import { db_name, db_user, db_password, db_host, db_url, db_environment } from './env.js'
 
-    
+    if(db_environment === "dev"){ console.log('DEV MODE') }
+
 //---- Config of database
     export const sequelize = (db_environment === "dev") 
         ? new Sequelize(db_name, db_user, db_password,{
             host:db_host,
             dialect:'mysql'
         }) 
-        : new Sequelize(db_url)
+        : new Sequelize(db_url,{ 
+            logging:false, // This desables the echo of queries to the database
+            pool: {
+                max: 5, // Max number of connections in the pool
+                min: 0, // Min number of connections in the pool
+                acquire: 30000, // Max waiting time in miliseconds for getting a connection of the pool (30 seconds)
+                idle: 15000, // Max idle time to automatically close the connection (15 seconds)
+              },
+        })
 
 
 
